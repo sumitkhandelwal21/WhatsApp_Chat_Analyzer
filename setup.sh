42ba8899c921301b3cff4ab4e8ac24e9590e0f44
Sumit Khandelwal <sumitguptadsa21@gmail.com>
2022-08-12 22:40:20 +0530

    changes made to the project

--- setup.sh
+++ setup.sh
@@ -1,6 +1,12 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[theme]\n\
+primaryColor='#e00a0a'\n\
+backgroundColor='#02370a'\n\
+secondaryBackgroundColor='#28bd2c'\n\
+textColor='#ffffff'\n\
+font='serif'\n\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
